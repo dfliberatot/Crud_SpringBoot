07cb5103fc4422ee0202fce5fbccda57e8816428
Brhayan <bliberatot@gmail.com>
2022-04-22 17:55:20 -0500

    Ajuste controlador

--- src/main/java/com/crud/angular/Controller/LoginController.java
+++ src/main/java/com/crud/angular/Controller/LoginController.java
@@ -3,8 +3,10 @@ package com.crud.angular.Controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +20,7 @@ import io.swagger.annotations.ApiOperation;
 @RestController
 @RequestMapping("/login")
 @Api(tags = "login", value = "/login")
+@CrossOrigin(origins = "*")
 public class LoginController {
 
 	
